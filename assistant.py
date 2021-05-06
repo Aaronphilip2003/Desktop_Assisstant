db46395a691d7688c207a4288ca6f81fd2633347
Aaronphilip2003 <69317200+Aaronphilip2003@users.noreply.github.com>
2021-05-06 23:37:32 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -3,6 +3,13 @@ import datetime
 import speech_recognition as sr
 import wheel
 import pyaudio
+import wikipedia
+import webbrowser
+import os
+import subprocess
+
+dir="D:\Spotify.exe.lnk"
+calcdir="C:\Windows\System32\calc.exe"
 
 engine=pyttsx3.init('sapi5')
 
@@ -50,8 +57,50 @@ def takeCommand():
 
 if __name__=="__main__":
     wishMe()
-    takeCommand()
 
 
-#ppip install pipwin
+    #Logic for executing tasks
+
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia...")
+            query=query.replace("wikipedia","")
+            results=wikipedia.summary(query,sentences=3)
+            speak("According to wikipedia")
+            speak(results)
+
+        elif 'youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'google' in query:
+            webbrowser.open("google.com")
+
+        elif 'github' in query:
+            webbrowser.open("github.com")
+
+        elif 'replit' in query:
+            webbrowser.open("replit.com")
+
+        elif 'spotify' in query:
+            os.startfile(dir)
+            os.system(dir)
+            # subprocess.Popen([dir])
+            # subprocess.call(dir)
+
+        elif 'calculator' in query:
+            pyttsx3.speak("Opening Calculator....")
+            os.startfile(calcdir)
+            # os.system(calcdir)
+
+
+        elif 'exit' or 'quit' or 'bye' in query:
+            pyttsx3.speak("Exiting...")
+            break
+
+
+
+
+
+#pip install pipwin
 #pipwin install pyaudio
